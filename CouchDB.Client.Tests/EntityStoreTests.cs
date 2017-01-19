370077a8720e28acde7702dd221619bea09a2c01
ttutisani <tengo_tutisani@hotmail.com>
2017-01-18 21:06:58 -0600

    Unit tested entity save operation.

--- CouchDB.Client.Tests/EntityStoreTests.cs
+++ CouchDB.Client.Tests/EntityStoreTests.cs
@@ -1,13 +1,124 @@
-﻿using System;
+﻿using Moq;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Threading.Tasks;
 using Xunit;
 
 namespace CouchDB.Client.Tests
 {
     public sealed class EntityStoreTests
     {
+        private readonly Mock<ICouchDBDatabase> _db;
+        private readonly EntityStore _sut;
+
+        public EntityStoreTests()
+        {
+            _db = new Mock<ICouchDBDatabase>();
+            _sut = new EntityStore(_db.Object);
+        }
+
+        [Fact]
         public void Ctor_Requires_DB()
         {
             Assert.Throws<ArgumentNullException>(() => new EntityStore(null));
+        }
+
+        private sealed class SampleEntity : IEntity
+        {
+            public string _id
+            {
+                get;
+                set;
+            }
+
+            public string _rev
+            {
+                get;
+                set;
+            }
+
+            public string Name { get; set; }
+            public int Name2 { get; set; }
+        }
+
+        [Fact]
+        public void SaveEntityAsync_Requires_Entity()
+        {
+            Assert.ThrowsAsync<ArgumentNullException>(() => _sut.SaveEntityAsync<SampleEntity>(null));
+        }
+
+        [Fact]
+        public void SaveEntityAsync_Removes_Rev_If_Empty()
+        {
+            //arrange.
+            var entity = new SampleEntity {
+                _id = "id123",
+                _rev = null,
+                Name = "some name",
+                Name2 = 123
+            };
+
+            var entityWithoutRev = new { entity._id, entity.Name, entity.Name2 };
+
+            _db.Setup(db => db.SaveDocumentAsync(It.IsAny<string>(), It.IsAny<DocUpdateParams>()))
+                .Returns(Task.FromResult(new SaveDocResponse(new CouchDBDatabase.SaveDocResponseDTO())));
+
+            //act.
+            _sut.SaveEntityAsync(entity).GetAwaiter().GetResult();
+
+            //assert.
+            _db.Verify(db => db.SaveDocumentAsync(It.Is<string>(str => StringIsJsonObject(str, JObject.FromObject(entityWithoutRev))), It.IsAny<DocUpdateParams>()), Times.Once());
+        }
+
+        [Fact]
+        public void SaveEntityAsync_Passes_Entity_And_Params()
+        {
+            //arrange.
+            var entity = new SampleEntity { _id = "id123", _rev = "rev123", Name = "somename", Name2 = 213 };
+            var entityJson = JsonConvert.SerializeObject(entity);
+            var updateParams = new DocUpdateParams();
+
+            _db.Setup(db => db.SaveDocumentAsync(It.IsAny<string>(), It.IsAny<DocUpdateParams>()))
+                .Returns(Task.FromResult(new SaveDocResponse(new CouchDBDatabase.SaveDocResponseDTO())));
+
+            //act.
+            _sut.SaveEntityAsync(entity, updateParams).GetAwaiter().GetResult();
+
+            //assert.
+            _db.Verify(db => db.SaveDocumentAsync(It.Is<string>(str => StringIsJsonObject(str, JObject.Parse(entityJson))), updateParams), Times.Once());
+        }
+
+        [Fact]
+        public void SaveEntityAsync_Sets_ID_And_Rev_After_Save()
+        {
+            //arrange.
+            var entity = new SampleEntity();
+            var expectedId = "idnew";
+            var expectedRev = "revnew";
+
+            _db.Setup(db => db.SaveDocumentAsync(It.IsAny<string>(), It.IsAny<DocUpdateParams>()))
+                .Returns(Task.FromResult(new SaveDocResponse(new CouchDBDatabase.SaveDocResponseDTO { Id = expectedId, Rev = expectedRev })));
+
+            //act.
+            _sut.SaveEntityAsync(entity, null).GetAwaiter().GetResult();
+
+            //assert.
+            Assert.Equal(expectedId, entity._id);
+            Assert.Equal(expectedRev, entity._rev);
+        }
+        
+        private static bool StringIsJsonObject(string value, JObject json)
+        {
+            if (value == null && json == null)
+                return true;
+
+            if (value == null || json == null)
+                return false;
+
+            var valueJson = JObject.Parse(value);
+
+            return JToken.DeepEquals(json, valueJson);
         }
     }
 }
