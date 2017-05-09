b77b360e11930fd153df299c86b826716ce1be63
ttutisani <tengo_tutisani@hotmail.com>
2017-05-08 20:50:24 -0500

    Bat files for running unit tests in console.

---
+++ CouchDB.Client.Tests/run-unit-tests-in-console-x32.bat
@@ -0,0 +1,3 @@
+cd %~dp0
+..\packages\xunit.runner.console.2.1.0\tools\xunit.console.exe bin\Debug\CouchDB.Client.Tests.dll
+pause
--- CouchDB.Client.Tests/run-unit-tests-in-console-x86.bat
+++ CouchDB.Client.Tests/run-unit-tests-in-console-x86.bat
@@ -1,4 +1,3 @@
-#..\packages\xunit.runner.console.2.1.0\tools\xunit.console.x86.exe bin\Debug\CouchDB.Client.Tests.dll
 cd %~dp0
 ..\packages\xunit.runner.console.2.1.0\tools\xunit.console.x86.exe bin\Debug\CouchDB.Client.Tests.dll
 pause
