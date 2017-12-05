04838d49de0fc23e7750d2c4fd16607e0ec26f4c
ttutisani <tengo_tutisani@hotmail.com>
2017-12-04 21:11:01 -0600

    Added thrown exception comments to ICouchDBDatabase interface too.

--- CouchDB.Client/Database/ICouchDBDatabase.cs
+++ CouchDB.Client/Database/ICouchDBDatabase.cs
@@ -15,6 +15,7 @@ namespace CouchDB.Client
         /// <param name="updateParams">Query parameters for updating document.</param>
         /// <returns><see cref="SaveDocResponse"/> with operation results in it.</returns>
         /// <exception cref="ArgumentNullException">Required parameter is null or empty.</exception>
+        /// <exception cref="CouchDBClientException">Error response received from CouchDB server.</exception>
         Task<SaveDocResponse> SaveStringDocumentAsync(string documentJsonString, DocUpdateParams updateParams = null);
 
         /// <summary>
@@ -25,6 +26,7 @@ namespace CouchDB.Client
         /// <param name="queryParams">Additional query parameters for retrieving document.</param>
         /// <returns><see cref="string"/> containing document JSON.</returns>
         /// <exception cref="ArgumentNullException">Required parameter is null or empty.</exception>
+        /// <exception cref="CouchDBClientException">Error response received from CouchDB server.</exception>
         Task<string> GetStringDocumentAsync(string docId, DocQueryParams queryParams = null);
 
         /// <summary>
@@ -38,6 +40,9 @@ namespace CouchDB.Client
         /// <param name="revision">Actual document’s revision.</param>
         /// <param name="batch">Stores document in batch mode Possible values: ok (when set to true). Optional.</param>
         /// <returns><see cref="SaveDocResponse"/> with operation results in it.</returns>
+        /// <exception cref="ArgumentNullException">Required parameter is null or empty.</exception>
+        /// <exception cref="InvalidOperationException">Delete request was already sent.</exception>
+        /// <exception cref="CouchDBClientException">Error response received from CouchDB server.</exception>
         Task<SaveDocResponse> DeleteDocumentAsync(string docId, string revision, bool batch = false);
 
         /// <summary>
@@ -48,6 +53,8 @@ namespace CouchDB.Client
         /// </summary>
         /// <param name="queryParams">Instance of <see cref="ListQueryParams"/> to be used for filtering.</param>
         /// <returns><see cref="DocListResponse{STRING}"/> containing list of JSON strings.</returns>
+        /// <exception cref="CouchDBClientException">Error response received from CouchDB server.</exception>
+        /// <exception cref="InvalidOperationException">Malformed JSON string received from CouchDB server..</exception>
         Task<DocListResponse<string>> GetAllStringDocumentsAsync(ListQueryParams queryParams = null);
 
         /// <summary>
@@ -59,18 +66,22 @@ namespace CouchDB.Client
         /// <param name="docIdList">Array of document IDs to be retrieved.</param>
         /// <param name="queryParams">Instance of <see cref="ListQueryParams"/> to be used for filtering.</param>
         /// <returns><see cref="DocListResponse{STRING}"/> containing list of JSON strings.</returns>
+        /// <exception cref="ArgumentNullException">Required parameter is null or empty.</exception>
+        /// <exception cref="CouchDBClientException">Error response received from CouchDB server.</exception>
+        /// <exception cref="InvalidOperationException">Malformed JSON string received from CouchDB server..</exception>
         Task<DocListResponse<string>> GetStringDocumentsAsync(string[] docIdList, ListQueryParams queryParams = null);
-        
+
         /// <summary>
         /// Allows you to create and update multiple documents at the same time within a single request. The basic operation is similar to creating or updating a single document, except that you batch the document structure and information.
-        /// When creating new documents the document ID(_id) is optional.
-        /// For updating existing documents, you must provide the document ID, revision information(_rev), and new document values.
+        /// When creating new documents the document ID (_id) is optional.
+        /// For updating existing documents, you must provide the document ID, revision information (_rev), and new document values.
         /// In case of batch deleting documents all fields as document ID, revision information and deletion status (_deleted) are required.
         /// </summary>
         /// <param name="documents">List of documents strings.</param>
         /// <param name="newEdits">If false, prevents the database from assigning them new revision IDs. Default is true. Optional</param>
         /// <returns>Instance of <see cref="SaveDocListResponse"/> with detailed information for each requested document to save.</returns>
         /// <exception cref="ArgumentNullException">Required parameter is null or empty.</exception>
+        /// <exception cref="CouchDBClientException">Error response received from CouchDB server.</exception>
         Task<SaveDocListResponse> SaveStringDocumentsAsync(string[] documents, bool newEdits = true);
 
         /// <summary>
@@ -81,6 +92,8 @@ namespace CouchDB.Client
         /// <param name="revision">Document revision. Required.</param>
         /// <param name="attachment">Attachment content.</param>
         /// <returns><see cref="SaveDocResponse"/> with operation results in it.</returns>
+        /// <exception cref="ArgumentNullException">Required parameter is null or empty.</exception>
+        /// <exception cref="CouchDBClientException">Error response received from CouchDB server.</exception>
         Task<SaveDocResponse> SaveAttachmentAsync(string docId, string attName, string revision, byte[] attachment);
 
         /// <summary>
@@ -90,6 +103,8 @@ namespace CouchDB.Client
         /// <param name="docId">Document ID.</param>
         /// <param name="attName">Attachment name.</param>
         /// <returns>Attachment content.</returns>
+        /// <exception cref="ArgumentNullException">Required parameter is null or empty.</exception>
+        /// <exception cref="CouchDBClientException">Error response received from CouchDB server.</exception>
         Task<byte[]> GetAttachmentAsync(string docId, string attName);
 
         /// <summary>
@@ -102,6 +117,8 @@ namespace CouchDB.Client
         /// <param name="batch">Store changes in batch mode Possible values: ok (when set to true). Optional.</param>
         /// <returns><see cref="SaveDocResponse"/> with operation results in it.</returns>
         /// <exception cref="ArgumentNullException">Required parameter is null or empty.</exception>
+        /// <exception cref="InvalidOperationException">Delete request was already sent.</exception>
+        /// <exception cref="CouchDBClientException">Error response received from CouchDB server.</exception>
         Task<SaveDocResponse> DeleteAttachmentAsync(string docId, string attName, string revision, bool batch = false);
     }
 }
