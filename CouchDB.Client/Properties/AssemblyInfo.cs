7283316c274a4fe7b5736ffff80ae99ec48261b4
ttutisani <tengo_tutisani@hotmail.com>
2017-12-06 21:21:43 -0600

    Final changes to nuget and publishe.

--- CouchDB.Client/Properties/AssemblyInfo.cs
+++ CouchDB.Client/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("CouchDBClient.NET")]
+[assembly: AssemblyTitle("CouchDBClient")]
 [assembly: AssemblyDescription("CouchDB .NET Client Library")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Tengiz Tutisani")]
-[assembly: AssemblyProduct("CouchDBClient.NET")]
+[assembly: AssemblyProduct("CouchDBClient")]
 [assembly: AssemblyCopyright("Copyright © Tengiz Tutisani 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
